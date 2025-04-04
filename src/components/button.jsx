4fdf74d1082e91c8704fbb756f2aaa499d05fa4b
6abr1elMR <gabriel.montes.github@gmail.com>
2025-04-04 12:57:39 -0500

    Feature: Add action for download cv

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,10 +1,16 @@
 import { PropTypes } from 'prop-types';
 
 const Button = ({ icon }) => {
-	const handleDownload = () => {
+	const handleDownload = async () => {
 		const urlCv =
 			'https://portfolio-docs-pbl.s3.us-east-1.amazonaws.com/VE-GabrielMontes-ES.pdf';
-		return window.open(urlCv, '_blank');
+		const link = document.createElement('a');
+		link.href = urlCv;
+		link.download = 'GabrielRodriguez-CV.pdf';
+		link.target = '_blank';
+		document.body.appendChild(link);
+		link.click();
+		document.body.removeChild(link);
 	};
 
 	return <button onClick={handleDownload}>{icon}Descargar CV</button>;
